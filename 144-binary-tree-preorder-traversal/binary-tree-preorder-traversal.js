de32ef6ae914ce3ed1a544e1f5db40535e289a84
Roshan Eldho Benny <129829990+roshannoonoottil@users.noreply.github.com>
2025-05-20 13:51:22 +0530

    Time: 0 ms (100.00%) | Memory: 53.3 MB (72.92%) - LeetSync

---
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+    let trav = []
+    function preorder(node){
+        if(!node) return
+        trav.push(node.val)
+        preorder(node.left)
+        preorder(node.right)
+    }
+        preorder(root)
+        return trav
+    
+};
