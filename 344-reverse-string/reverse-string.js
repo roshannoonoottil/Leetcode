d14a6cf6a40a56f73d20370a291908c19ef82c70
Roshan Eldho Benny <129829990+roshannoonoottil@users.noreply.github.com>
2025-07-10 13:48:10 +0530

    Time: 1 ms (55.29%) | Memory: 60.8 MB (61.79%) - LeetSync

--- 344-reverse-string/reverse-string.js
+++ 344-reverse-string/reverse-string.js
@@ -3,5 +3,10 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    return s.reverse()
+    for(let i = 0;i<Math.floor(s.length/2);i++){
+        let temp = s[i]
+        s[i] = s[s.length - 1 - i]
+        s[s.length - 1 - i] = temp
+    }
+    
 };
